76cbf7f2f0b6095f9ba186a1742d3427262bfe7c
Victor Momodu <victoramomodu@gmail.com>
2025-06-19 03:31:29 +0100

    feat: initial commit to React Router v7

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,8 @@
-import { fileURLToPath, URL } from 'node:url'
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
-})
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+});
