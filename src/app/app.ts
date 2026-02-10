82c4ed7a11c47ed1ca39c5f204d54a0ad8231695
Victor Momodu <victoramomodu@gmail.com>
2026-02-10 13:47:15 +0100

    feat: rename vendoor to bizz

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('vendoor-frontend');
+  protected readonly title = signal('bizz-frontend');
 }
