55a3ba0077f45c790489fbe915a1f00be14abfbc
Victor Momodu <victoramomodu@gmail.com>
2026-02-13 20:17:14 +0100

    feat: setup homepage components

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,3 +1,19 @@
+import CallsToAction from "./components/CallsToAction/CallsToAction";
+import Features from "./components/Features/Features";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import Statistics from "./components/Statistics/Statistics";
+
 export default function Home() {
-    return <h1>Welcome home!</h1>;
+    return (
+        <>
+            <Header />
+            <Hero />
+            <CallsToAction />
+            <Features />
+            <Statistics />
+            <Footer />
+        </>
+    );
 }
