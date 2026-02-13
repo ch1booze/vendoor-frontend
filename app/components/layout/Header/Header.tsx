c10d295e54cab4c8e013cddc3894fd0e8a53f309
Victor Momodu <victoramomodu@gmail.com>
2026-02-13 23:20:01 +0100

    feat: create Header component

--- app/components/layout/Header/Header.tsx
+++ app/components/layout/Header/Header.tsx
@@ -1,3 +1,20 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Header() {
-  return <>Header</>;
+    return (
+        <header>
+            <Image src="/logo.png" alt="Logo" width={100} height={50} />
+            <h2>bizz</h2>
+            <nav>
+                <Link href="/">Features</Link>
+                <Link href="/">Pricing</Link>
+                <Link href="/">About</Link>
+                <Link href="/">Contact</Link>
+            </nav>
+
+            <button>Login</button>
+            <button>Get Started</button>
+        </header>
+    );
 }
